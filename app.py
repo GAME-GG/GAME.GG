8f7b19ec95e78655a3ec68840530340f08914285
civilcoy <joungcb06@gmail.com>
2023-01-17 05:06:04 +0900

    .decode('utf-8') 주석 처리 : 배포시 주석 해제할것

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ def api_login():
             'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        # 배포 시 )..decode('utf-8') 붙이기!
 
         # token을 줍니다.
         return jsonify({'result': 'success', 'token': token})
